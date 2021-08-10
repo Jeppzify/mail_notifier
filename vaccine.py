4d0eae400744adbb800a3ad7eb5414bba5644701
Jeppzify <30358279+Jeppzify@users.noreply.github.com>
2021-08-10 15:30:47 -0400

    Update vaccine.py
    Refactored and more generalized. Password as cmdline arg.

--- vaccine.py
+++ vaccine.py
@@ -4,24 +4,26 @@ import re
 import smtplib
 import ssl
 import requests
+import sys
 
+# For testing:
 #port = 25
 #smtp_server = "localhost"
+
 port = 587
 smtp_server = "smtp.gmail.com"
 sender = "jscds07@gmail.com"
-receivers = ["jscds07@gmail.com", "stangertz_6@hotmail.com"]
-recv2 = "stangertz_6@hotmail.com"
-password = "Uzs7CRZ7"
+receivers = "jscds07@gmail.com"
+password = sys.argv[1]
 context = ssl.create_default_context()
 
 
 message = """\
-	Subject: Vaccin-check
-	To: jscds07@gmail.com,stangertz_6@hotmail.com
+	Subject: BotAlert!
+	To: jscds07@gmail.com
 	From: jscds07@gmail.com
 
-	Boka vaccin nu!
+	Message from bot
 """
 
 def mail_me():
@@ -39,35 +41,16 @@ def mail_me():
 	except smtplib.SMTPException as e:
 		print('SMTP error occurred: ' + str(e))
 
-def mail_billie():
-	try:
-		#send your message with credentials specified above
-		server = smtplib.SMTP(smtp_server, port)
-		server.starttls(context=context)
-		server.login(sender, password)
-		server.sendmail(sender, recv2, message)
-
-		# tell the script to report if your message was sent or which errors need to be fixed
-		print('Sent')
-	except smtplib.SMTPServerDisconnected:
-		print('Failed to connect to the server. Wrong user/password?')
-	except smtplib.SMTPException as e:
-		print('SMTP error occurred: ' + str(e))
-
 r = requests.get("https://www.skane.se/Halsa-och-vard/hitta-vard/covid-19-coronavirus/fragor-och-svar-om-covid-19-vaccination/lediga-tider-for-vaccination-mot-covid-19/")
 
 resp = r.text
+
 pattern = "<a href=\"https\:\/\/.*Boka\stid\shos\sSvea.*</a>"
 pattern_two = "SE162321000255-O18986"
 
-svea = re.findall(pattern, resp)
-#print(result)
-berga = re.findall(pattern_two, resp)
-if "Helsingborg" in svea:
+site1 = re.findall(pattern, resp)
+site2 = re.findall(pattern_two, resp)
+if "Helsingborg" in site1 or pattern_two in site2:
 	mail_me()
-	mail_billie()
-elif pattern_two in berga:
-	mail_me()
-	mail_billie()
 else:
 	exit()
